f04ab4a5e3bef6ae9875553635a381f8d967bb72
iamraihan <trueraihan@gmail.com>
2022-05-23 11:40:42 +0600

    purchase page make requre

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Home/Shared/Footer';
 import Login from './components/Home/Shared/Login';
 import Navbar from './components/Home/Shared/Navbar';
 import Register from './components/Home/Shared/Register';
+import RequireAuth from './components/Home/Shared/RequireAuth';
 import Purchase from './components/Purchase/Purchase';
 
 function App() {
@@ -13,7 +14,11 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/purchase' element={<Purchase></Purchase>}></Route>
+        <Route path='/purchase' element={
+          <RequireAuth>
+            <Purchase></Purchase>
+          </RequireAuth>
+        }></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register />}></Route>
       </Routes>
