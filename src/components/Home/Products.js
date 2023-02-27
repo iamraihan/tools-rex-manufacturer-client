08e8ec5e95b9f4c878c24c9178b427b358089671
iamraihan <trueraihan@gmail.com>
2023-02-27 22:11:51 +0600

    if api failed show a msg

--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -10,12 +10,22 @@ const Products = () => {
   }, []);
 
   return (
-    <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 max-w-screen-xl mx-auto">
-      {
-        products.map((product) => (
-          <Product key={product._id} product={product}></Product>
-        )) // need to recheck
-      }
+    <div>
+      {!products.length && (
+        <p
+          className="text-red-500 text-4xl text-center font-semibold
+      "
+        >
+          If the tool doesn't show please reload again
+        </p>
+      )}
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 max-w-screen-xl mx-auto">
+        {
+          products.map((product) => (
+            <Product key={product._id} product={product}></Product>
+          )) // need to recheck
+        }
+      </div>
     </div>
   );
 };
